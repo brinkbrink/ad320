eb10401e9730651dccb5ae4d26f588c5b04b7a67
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2023-03-19 17:38:31 -0700

    whew

--- findAny/findAny.mjs
+++ findAny/findAny.mjs
@@ -2,10 +2,7 @@ import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from 'cors';
-dotenv.config()
-// Replace the uri string with your connection string.
-// console.log(process.env)
-// const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+dotenv.config();
 const db_username = process.env.MONGO_DB_USERNAME;
 const db_password = process.env.MONGO_DB_PASSWORD;
 const db_url = process.env.MONGO_DB_URL;
@@ -14,17 +11,17 @@ const uri =
 const client = new MongoClient(uri);
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.set('port', process.env.PORT || 3000);
-app.get('/findAny', async (req,res) => {
-  const {chosenDatabase, chosenCollection, filter, projection} = req.body;
+app.post('/findAny', async (req,res) => {
+    const {database, collection, filter, options} = req.body;
     try {
-      
+        
         await client.connect();
-
-     
-        const db = client.db(chosenDatabase);
-        const collect = db.collection(chosenCollection);
-        const result = await collect.findOne(filter, {projection});
+        const options = {projection: projection}
+        const db = client.db(database);
+        const collect = db.collection(collection);
+        const result = await collect.findOne(filter, options);
 
         res.type('json');
         res.status(200);
--- findAny/findOne.mjs
+++ findAny/findOne.mjs
@@ -14,36 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.set('port', process.env.PORT || 3000);
-app.post('/findOne', async function handler (req,res)  {
-// console.log(req.body);
-// console.log(req.body.database);
-// console.log(req.body.collection);
-// let database = req.body.database;
-// console.log(database);
-//         const db = client.db(req.body.database);
-//         const collect = db.collection(req.body.collection);
-//         const result = collect.findOne();
-//         console.log(result);
-    // try {
-      
-    //     // await client.connect();
-
-    //     // const options = {projection: projection}
-    //     const db = client.db(req.body.database);
-    //     const collect = db.collection(req.body.collection);
-    //     const result = await collect.findOne();
-
-    //     // res.type('json');
-    //     res.status(200);
-    //     res.json(result);
-    //     // console.log(req.body);
-    // } catch (error) {
-    //   console.log(error);  
-    // } finally {
-    //     // Ensures that the client will close when you finish/error
-    //     await client.close();
-    // // }
-    // res.send();
+app.post('/findOne', async function find (req,res)  {
   try {
     await client.connect();
     const db = client.db(req.body.database);
